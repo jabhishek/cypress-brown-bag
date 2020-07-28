564ebd88fbfb97e69f008a8d3ef040b9d80935a3
Abhishek Jain <abhi2000@gmail.com>
2020-07-28 10:58:31 +0100

    initial commit

---
+++ src/container/HomePage.js
@@ -0,0 +1,12 @@
+import { Page } from "../components/Page";
+import React from "react";
+
+export const Home = () => {
+  return (
+    <Page>
+      <h1>Home</h1>
+    </Page>
+  );
+};
+
+export default Home;
---
+++ src/container/NotFound.js
@@ -0,0 +1,12 @@
+import { Page } from "../components/Page";
+import React from "react";
+
+export const NotFound = () => {
+  return (
+    <Page>
+      <h1>NotFound!!</h1>
+    </Page>
+  );
+};
+
+export default NotFound;
